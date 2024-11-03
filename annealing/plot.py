fe4261f3ce1f27a7095ced8282dc870707d96b48
qrst0 <66962233+qrst0@users.noreply.github.com>
2024-11-03 21:39:20 +0700

    feat: annealing

---
+++ annealing/plot.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+filename = sys.argv[1]
+f = sys.argv[2]
+
+intOk = True
+if f == "F":
+    intOk = False
+ 
+print(filename, intOk)
+
+xpoints = []
+ypoints = []
+
+firstLine = True
+
+# Open the file in read mode
+with open(filename, 'r') as file:
+    # Read each line in the file
+    for line in file:
+                
+        if firstLine:
+            firstLine = False
+            continue
+        
+        # Print each line
+        rl = line.strip()
+        spl = rl.split()
+        
+        xpoints.append(int(spl[1]))
+        
+        if not intOk:
+            ypoints.append(float(spl[0]))
+        else:
+            ypoints.append(int(spl[0]))
+        # print(spl[0], spl[1])
+
+xpoints = np.array(xpoints)
+ypoints = np.array(ypoints)
+
+plt.plot(xpoints, ypoints)
+plt.show()
